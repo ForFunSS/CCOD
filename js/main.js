54f3ad6877223c7e21fc3f99e6f98794d77c26d6
ForFunSS <blitsya@gmail.com>
2021-07-06 13:50:29 +0300

    commit edit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 
-$('.really-big-slider').slick({
+$('.front-big-slider').slick({
     dots:true,
     arrows:false,
-    
+    centerMode: true,
+    slidesToShow:1,
+    variableWidth: true,
+
+    breakpoint: 1530,
+        settings: {
+          slidesToShow: 2,
+        }
 });
 
