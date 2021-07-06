f526566a15338dcde5a61f65638e562d9debeed5
ForFunSS <blitsya@gmail.com>
2021-07-06 18:23:49 +0300

    no responsive

--- js/main.js
+++ js/main.js
@@ -2,13 +2,7 @@
 $('.front-big-slider').slick({
     dots:true,
     arrows:false,
-    centerMode: true,
-    slidesToShow:1,
-    variableWidth: true,
-
-    breakpoint: 1530,
-        settings: {
-          slidesToShow: 2,
-        }
+    infinite:false,
+    accessibility: false,
 });
 
