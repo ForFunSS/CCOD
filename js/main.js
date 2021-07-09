b526330deab433c100a00e15bd1665028462263d
ForFunSS <blitsya@gmail.com>
2021-07-09 18:53:55 +0300

    new responsive

--- js/main.js
+++ js/main.js
@@ -6,4 +6,28 @@ $('.front-big-slider_js').slick({
     accessibility: false,
 });
 
-//.front-big-slider_js 
+var scrollPrev = 0;
+        $(window).scroll(function () {
+            var scrolled = $(this).scrollTop(),
+                header_height = $('.header_navig').height();
+            if (scrolled > header_height) {
+                $('.site-header').addClass('hide_menu');
+            } else {
+                $('.site-header').removeClass('hide_menu');
+            }
+
+            if (scrollPrev > scrolled) {
+                $('.site-header').removeClass('hide_menu');
+            }
+            scrollPrev = scrolled;
+        });
+
+$('.burger').click(function () { 
+    $('.main-menu').toggleClass('hide-burger');
+    $('.close-button').toggleClass('invis');
+});
+
+$('.close').click(function () { 
+    $('.main-menu').removeClass('hide-burger');
+    $('.close-button').toggleClass('invis');
+});
